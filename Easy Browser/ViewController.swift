3a8e2905d12d8802211f1f122bce27b17f4d1823
Nikolai Saganenko <9952728@gmail.com>
2021-10-24 11:45:42 +0300

    Add UIBarButtonItem Add UIAlertAction

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -22,11 +22,35 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Easy Browser"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         let url = URL(string: "https://google.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
-
-
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page:", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "mail.ru", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        // For Ipad
+//        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = "Easy Browser: " + "\(webView.title ?? "")"
+    }
+    
 }
 
