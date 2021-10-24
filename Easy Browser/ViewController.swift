aefd68851dda8389f78ee688c3beae3546d2a662
Nikolai Saganenko <9952728@gmail.com>
2021-10-24 12:31:08 +0300

    Add UIProgressView Add KVO Add decisionHandler

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -6,16 +6,25 @@
 //
 
 import UIKit
+
+// we need this to use WKWebView etc.
 import WebKit
 
+// Don't forget WKNavigationDelegate protocol
 class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    
+    // Safe list of web sites
+    var websites = ["google.com", "apple.com", "mail.ru"]
     
     override func loadView() {
         
         webView = WKWebView()
         webView.navigationDelegate = self
+        
+        // ViewController's view become a browser
         view = webView
     }
 
@@ -24,32 +33,91 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         title = "Easy Browser"
         
+        // adds "open" nav bar item
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
-        let url = URL(string: "https://google.com")!
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        // adds "refresh" bar button
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        // adds progress view
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        
+        // shows toolbar at bottom
+        navigationController?.isToolbarHidden = false
+        
+        // adds observer to see website's loading progress
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+        // Starting page
+        let url = URL(string: "https://\(websites[0])")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
     
     @objc func openTapped() {
+        
+        // Create an instance of alert controller
         let ac = UIAlertController(title: "Open page:", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "mail.ru", style: .default, handler: openPage))
+        
+        // Add our safe-list sites
+        for site in websites {
+            ac.addAction(UIAlertAction(title: site, style: .default, handler: openPage))
+        }
+
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
-        // For Ipad
+        // Uncomments this line if you use Ipad
 //        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        
+        // launch alert controller
         present(ac, animated: true)
     }
     
+    // load page with url
     func openPage(action: UIAlertAction) {
         let url = URL(string: "https://" + action.title!)!
         webView.load(URLRequest(url: url))
     }
     
+    // Update Nav title
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = "Easy Browser: " + "\(webView.title ?? "")"
+    }
+    
+    // Adds KVO (key value observer) for estimatedProgress
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            // update progressView
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    
+    // Delete this func if dont want any resrictions
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        // set url to be equal navigation url
+        let url = navigationAction.request.url
+        
+        // unwrap url's host
+        if let host = url?.host {
+            // check if it is on our safe list
+            for website in websites {
+                if host.contains(website){
+                    // allow loading
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        // else cancel loading
+        decisionHandler(.cancel)
     }
     
 }
